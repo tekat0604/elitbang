be8da2706995c21db974664e13b8709f0c158290
Axxell1720 <gusticaesar17@gmail.com>
2026-07-30 13:29:37 +0700

    tambah popup passkey tanda tangan

--- resources/views/livewire/penandatangan/brida/tanda-tangan-list.blade.php
+++ resources/views/livewire/penandatangan/brida/tanda-tangan-list.blade.php
@@ -32,9 +32,8 @@
                 </td>
                 <td class="text-center"><a href="{{ route('penandatangan.brida.detail', $item->id) }}"
                     class="btn btn-sm btn-outline-primary"><i class="fas fa-file-alt me-1"></i> Detail</a></td>
-                <td class="text-center"><a
-                    href=""
-                    class="btn btn-sm btn-primary">Tanda Tangan <i class="fas fa-pen-nib ms-1"></i></a></td>
+                <td class="text-center"><button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal"
+                    data-bs-target="#modalKonfirmasiTandaTangan">Tanda Tangan <i class="fas fa-pen-nib ms-1"></i></button></td>
               </tr>
             @empty
               <tr>
@@ -46,5 +45,26 @@
       </div>
     </div>
     <div class="card-footer bg-white px-4 py-3 border-top">{{ $permohonanList->links() }}</div>
+  </div>
+
+  <div class="modal fade" id="modalKonfirmasiTandaTangan" tabindex="-1" aria-labelledby="modalKonfirmasiTandaTanganLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalKonfirmasiTandaTanganLabel"><i class="fas fa-shield-alt me-2"></i>Konfirmasi Tanda Tangan</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
+        </div>
+        <div class="modal-body">
+          <p class="mb-3">Masukkan password akun Anda untuk melanjutkan proses tanda tangan elektronik.</p>
+          <label for="password-tanda-tangan" class="form-label fw-semibold">Password</label>
+          <input type="password" class="form-control" id="password-tanda-tangan" placeholder="Masukkan password" autocomplete="current-password">
+          <div class="form-text">Password akan diverifikasi saat proses tanda tangan diaktifkan.</div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Batal</button>
+          <button type="button" class="btn btn-primary"><i class="fas fa-pen-nib me-1"></i>Lanjutkan Tanda Tangan</button>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
